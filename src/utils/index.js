02e665c184f6cccd72e124dde0caa7835cc0ad2f
Garcia William <william.garcia@partner.bmwgroup.com>
2018-10-04 14:41:29 -0400

    feature: website URL must be HTTPS

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,5 @@
 export const validateUrl = (url) => {
-  const urlRegex = /^(https:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}[\.]{0,1}/;
+  const urlRegex = /^https:\/\/(www\.)?[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,10}$/;
   if (urlRegex.test(url)) {
     return true;
   }
